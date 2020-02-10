131a3ed504432eebfa0088447d652066fcffba72
Jack Stehn <jack.stehn@berkeley.edu>
2020-02-10 15:55:52 -0800

    Made find_cases that takes in a year and gets a table of case information

--- find_cases.py
+++ find_cases.py
@@ -1,12 +1,24 @@
 import requests
 from bs4 import BeautifulSoup as bs
+import pandas as pd
 
-def find_cases(url):
+def find_cases(year):
+    url = "https://caselaw.findlaw.com/court/us-supreme-court/years/" + year
     page = requests.get(url)
     soup = bs(page.content, 'html.parser')
+    table = soup.find('table', attrs={'class': 'responsive-card-table unstriped'})
+    table_heads = table.find_all('th')
+    table_rows = table.find_all('tr')
+    links = table.find_all('a', href=True)
+    links = [link.get("href") for link in links]
+    l = []
+    for i in range(len(table_rows)):
+        td = table_rows[i].find_all('td')
+        row = [tr.text for tr in td]
+        l.append(row)
+    l.pop(0)
+    df = pd.DataFrame(l)
+    df['Link'] = links
+    return(df)
 
-    descriptions = soup.find_all("td",{"data-label" : "Description" })
-    for description in descriptions:
-        print(description.text)
-
-find_cases("https://caselaw.findlaw.com/court/us-supreme-court/years/2018")
+print(find_cases("2018"))
